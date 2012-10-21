f749a83385595543a758d41acb99c031e3aff949
Brian <Brian@192.168.1.123>
2012-10-21 10:30:06 -0500

    Changes to lab 3

--- src/class17lab/Lab3.java
+++ src/class17lab/Lab3.java
@@ -15,39 +15,12 @@ public class Lab3 {
         
         Map<String,Employee> map = new HashMap<>();
         
-        //seems to allow you to put different objects in the same spots.  
-        //Weird.  It looks like it just overwrites the last object that
-        // was put in that place.  Maybe that is why they used put instead
-        // of add, since you can put an object in a map, but the map size does
-        // not grow.
         map.put(e1.getSsn(), e1);
         map.put(e2.getSsn(), e2);
         map.put(e3.getSsn(), e3);
         map.put(e4.getSsn(), e4);
 
-        ArrayList<Employee> employees = new ArrayList<>();
-        
-        
-        //first attempt at listing.  I already know the key.
-        // but there is a problem.  There are duplicates beacuse I didn't
-        // put the keys in the set.  And sometimes I wouldn't know what the
-        // key is.
-        employees.add(map.get(e1.getSsn()));
-        employees.add(map.get(e2.getSsn()));
-        employees.add(map.get(e3.getSsn()));
-        employees.add(map.get(e4.getSsn()));
-        
-        System.out.println(map.size());
-        System.out.println("");
-        
-        for (Employee e: employees){
-            System.out.println(e);
-        }
-        
-        // looping through the keys.  A better way
         Set<String> keys = map.keySet();
-        
-        System.out.println("");
         
         for (String key : keys){
             System.out.println(map.get(key));
